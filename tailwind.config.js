cc1647fb224204051783d8f044782026285ff919
abolphazl <gorgdis@gmail.com>
2023-02-25 14:11:21 +0330

    change UI

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: ["./templates/**/*.{html,htm}"],
-    theme: {
-        extend: {},
+  content: ["./templates/**/*.{html,htm}"],
+  theme: {
+    container: {
+      padding: {
+        DEFAULT: '1rem',
+        sm: '2rem',
+        lg: '4rem',
+        xl: '5rem',
+        '2xl': '6rem',
+      },
     },
-    plugins: [],
+  },
+  plugins: [],
 }
